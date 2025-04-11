254eeed49979457560585f4f7f47e2ad4c89006d
Gustavo Lourenco da Silva <gustavolsilva@outlook.com>
2025-04-11 20:44:47 -0300

    feat: include files about functions python and a doc.md

---
+++ aula_funcoes_python/00_primeira_funcao.py
@@ -0,0 +1,15 @@
+# %%
+def exibir_mensagem():
+    print("Olá, Mundo!")
+
+def exibir_mensagem_2(nome):
+    print(f"Seja bem vindo {nome}!")
+
+def exibir_mensagem_3(nome="Anônimo"):
+    print(f"Seja bem vindo {nome}!")
+
+# %%
+# exibir_mensagem()
+# exibir_mensagem_2(nome="Gustavo")
+# exibir_mensagem_3()
+# exibir_mensagem_3(nome="Chappie")
